460ffb525cbb39056b1e3a59533083260f8b71ef
sharfinaega <ega.sharfina@gmail.com>
2019-11-18 10:54:13 +0700

    Updates

---
+++ precache-manifest.79836ecbaa3e03dab237d5e4e9d500f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584629b40d4b035c1fa852b374d5b912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd52bf14e445341fea3",
+    "url": "/static/css/main.fdb282d7.chunk.css"
+  },
+  {
+    "revision": "0a8b0040d9e51404b2dd",
+    "url": "/static/js/2.a00cf423.chunk.js"
+  },
+  {
+    "revision": "7bd52bf14e445341fea3",
+    "url": "/static/js/main.54f939a6.chunk.js"
+  },
+  {
+    "revision": "b63362eb8be8605a4c5d",
+    "url": "/static/js/runtime-main.37e58f78.js"
+  }
+]);
